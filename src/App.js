066f2b351784789c4687a84885f05c7fa7629a5a
hColleen <hcolleen@gmail.com>
2018-10-06 04:06:21 -0700

    start marker creation

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ import SquareAPI from './API/';
 import Map from './component/Map'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      venues: [],
+      markers: []
+    }
+  }
+
   componentDidMount(){
     SquareAPI.search({
       ll: "33.42,-111.83",
       query: 'coffee'
-    }).then(results => console.log(results))
-  }
+    }).then(results =>{
+      const { venues } = results.response;
+      const { markers } = venues.map(venue => {
+        return{
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true
+        }
+      })
+      this.setState({ venues, markers })
+      console.log(results)
+ })
+    }
   
   render() {
     return (
--- src/component/Map.js
+++ src/component/Map.js
@@ -7,18 +7,20 @@ class Map extends Component{
     }
 
     renderMap = () =>{
-        loadMapAPI("https://maps.googleapis.com/maps/api/js?key=AIzaSyCnPeVOPbLkPtwjEbH9MKDppTkoFSVmKdA&callback=initMap")
-        window.initMap = this.initMap
-      }
+            loadMapAPI("https://maps.googleapis.com/maps/api/js?key=AIzaSyCnPeVOPbLkPtwjEbH9MKDppTkoFSVmKdA&callback=initMap")
+            window.initMap = this.initMap
+        }
 
     initMap = () => {
-        const styles = [{"featureType": "all", "elementType": "all", "stylers": [{"hue": "#0000b0"},{"invert_lightness": "true"},{"saturation": -30}]}]
-        const map = new window.google.maps.Map(document.getElementById('map'), {
-          center: {lat: 33.415076, lng: -111.831389},
-          zoom: 16,
-          styles: styles
-        })
-    }
+            const styles = [{"featureType": "all", "elementType": "all", "stylers": [{"hue": "#0000b0"},{"invert_lightness": "true"},{"saturation": -30}]}]
+            const map = new window.google.maps.Map(document.getElementById('map'), {
+            center: {lat: 33.415076, lng: -111.831389},
+            zoom: 16,
+            styles: styles
+            })
+
+            let marker = new window.google.maps.Marker({})
+        }
 
     render(){
         return(
